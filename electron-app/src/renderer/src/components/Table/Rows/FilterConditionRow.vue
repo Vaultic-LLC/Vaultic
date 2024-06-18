08a3472da28619e6f0fc8afb47e29049266dbea0
Tyler Wanta <39772698+TylerWanta@users.noreply.github.com>
2024-06-18 15:56:34 -0500

    Fix filter condition row value width difference

--- electron-app/src/renderer/src/components/Table/Rows/FilterConditionRow.vue
+++ electron-app/src/renderer/src/components/Table/Rows/FilterConditionRow.vue
@@ -10,12 +10,16 @@
 				<EnumInputField :label="'Condition'" :color="color" v-model="filterCondition.filterType"
 					:optionsEnum="filterConditionType" fadeIn="true" :isOnWidget="true" :width="'8vw'" :height="'4vh'"
 					:minHeight="'35px'" :minWidth="'100px'" :maxHeight="'50px'" />
-				<TextInputField v-if="inputType == 0" :label="'Value'" :color="color" v-model="filterCondition.value"
-					:fadeIn="true" :isOnWidget="true" :width="'8vw'" :height="'3.8vh'" :minHeight="'33px'"
-					:minWidth="'100px'" :maxHeight="'49px'" />
-				<EnumInputField v-if="inputType == 1" :label="'Value'" :color="color" v-model="filterCondition.value"
-					:optionsEnum="inputEnumType" fadeIn="true" :isOnWidget="true" :width="'8vw'" :height="'4vh'"
-					:minHeight="'35px'" :minWidth="'100px'" :maxHeight="'50px'" />
+				<!-- the EnumInputField is like 1.3px wider than the TextInputField at the same width, causing the other inputs to
+				 shift slightly when changing. Wrap them in a div with fixed width and set their width to 100% to fix this -->
+				<div class="filterConditionCell__valueInput">
+					<TextInputField v-if="inputType == 0" :label="'Value'" :color="color"
+						v-model="filterCondition.value" :fadeIn="true" :isOnWidget="true" :width="'100%'"
+						:height="'3.8vh'" :minHeight="'33px'" :minWidth="'100px'" :maxHeight="'49px'" />
+					<EnumInputField v-if="inputType == 1" :label="'Value'" :color="color"
+						v-model="filterCondition.value" :optionsEnum="inputEnumType" fadeIn="true" :isOnWidget="true"
+						:width="'100%'" :height="'4vh'" :minHeight="'35px'" :minWidth="'100px'" :maxHeight="'50px'" />
+				</div>
 			</div>
 		</td>
 	</TableRow>
@@ -121,5 +125,9 @@ export default defineComponent({
 	.filterConditionRowSpace {
 		height: 5px;
 	}
+}
+
+.filterConditionCell__valueInput {
+	width: 8vw;
 }
 </style>
