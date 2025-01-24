6fd99a59ff45f41f6f664171b8d77656b9ea356a
Tyler Wanta <39772698+TylerWanta@users.noreply.github.com>
2025-01-24 07:25:57 -0600

    tooltips to side drawer

--- core-renderer/src/components/SideDrawer.vue
+++ core-renderer/src/components/SideDrawer.vue
@@ -17,7 +17,8 @@
         </div>
         <div class="sideDrawer__vaultList">
             <TreeList :nodes="allNodes" @onAdd="openCreateVaultPopup" :onLeafClicked="onLeafClicked">
-                <VaulticButton :color="primaryColor" :preferredSize="'1vw'" :minSize="'15px'" @click="syncVaults">
+                <VaulticButton :color="primaryColor" :preferredSize="'1vw'" :minSize="'15px'" :tooltipMessage="'Sync all Vaults'" 
+                    @click="syncVaults">
                     <ion-icon name="sync-outline"></ion-icon>                
                 </VaulticButton>
             </TreeList>
