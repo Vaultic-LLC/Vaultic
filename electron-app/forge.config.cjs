c46a1f05bccc6a2686f6b430cf48f9223d3d94d2
Tyler Wanta <fuzzy1170@gmail.com>
2025-09-04 06:49:56 -0400

    make for mas

--- electron-app/forge.config.cjs
+++ electron-app/forge.config.cjs
@@ -1,9 +1,67 @@
 module.exports = {
 	packagerConfig: {
+		asar: true,
+		appBundleId: 'com.vaultic.vaultic',
+		appVersion: '1.0.0',
+		buildVersion: '1.0.0',
+		icon: './resources/icon',
 		ignore: [
 			/^\/src/,
 			/(.eslintrc.json)|(.gitignore)|(electron.vite.config.ts)|(forge.config.cjs)|(tsconfig.*)/,
 		],
+		osxSign: {
+			identity: 'Apple Distribution: Vaultic LLC (YU69N454M8)',
+			platform: 'mas',
+			type: 'distribution',
+			provisioningProfile: 'Vaultic_Mac_App_Distribution.provisionprofile',
+			entitlements: './entitlements.plist',
+			// optionsForFile: (filePath) => {
+			// 	if (filePath.endsWith('Vaultic.app')) 
+			// 	{
+			// 		return {
+			// 			entitlements: path.resolve(__dirname, './entitlements.plist'),
+			// 			hardenedRuntime: true,
+			// 		}
+			// 	}
+
+			// 	if (filePath.endsWith('Vaultic Helper (GPU).app')) 
+			// 	{
+			// 		return {
+			// 			entitlements: path.resolve(__dirname, './entitlements.gpu.plist'),
+			// 			hardenedRuntime: true,
+			// 		}
+			// 	}
+
+			// 	if (filePath.endsWith('Vaultic Helper (Plugin).app')) 
+			// 	{
+			// 		return {
+			// 			entitlements: path.resolve(__dirname, './entitlements.plugin.plist'),
+			// 			hardenedRuntime: true,
+			// 		}
+			// 	}
+
+			// 	if (filePath.endsWith('Vaultic Helper (Renderer).app')) 
+			// 	{
+			// 		return {
+			// 			entitlements: path.resolve(__dirname, './entitlements.renderer.plist'),
+			// 			hardenedRuntime: true,
+			// 		}
+			// 	}
+
+			// 	if (filePath.endsWith('Vaultic Helper.app')) 
+			// 	{
+			// 		return {
+			// 			entitlements: path.resolve(__dirname, './entitlements.renderer.plist'),
+			// 			hardenedRuntime: true,
+			// 		}
+			// 	}
+
+			// 	return {
+			// 		entitlements: path.resolve(__dirname, './entitlements.plist'),
+			// 		hardenedRuntime: true,
+			// 	}
+			// }
+		}
 	},
 	rebuildConfig: {},
 	makers: [
@@ -11,10 +69,10 @@ module.exports = {
 			name: '@electron-forge/maker-squirrel',
 			config: {},
 		},
-		{
-			name: '@electron-forge/maker-zip',
-			platforms: ['darwin'],
-		},
+		// {
+		// 	name: '@electron-forge/maker-zip',
+		// 	platforms: ['darwin'],
+		// },
 		{
 			name: '@electron-forge/maker-deb',
 			config: {},
@@ -23,5 +81,11 @@ module.exports = {
 			name: '@electron-forge/maker-rpm',
 			config: {},
 		},
+		{
+			name: '@electron-forge/maker-pkg',
+			config: {
+			  identity: '3rd Party Mac Developer Installer: Vaultic LLC (YU69N454M8)'
+			}
+		  }
 	],
 };
